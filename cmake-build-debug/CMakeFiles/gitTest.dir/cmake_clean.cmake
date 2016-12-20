11a84b771319a728eac6d919634316b3a221b1f3
CUI Shidong <CUI@CUIs-MacBook-Pro.local>
2016-12-20 18:58:56 +0800

    inital commit

---
+++ cmake-build-debug/CMakeFiles/gitTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gitTest.dir/main.cpp.o"
+  "gitTest.pdb"
+  "gitTest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gitTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/gitTest.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for gitTest.
+# This may be replaced when dependencies are built.
